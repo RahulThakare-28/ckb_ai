90233c4a581bdd0f934f4f42bc1a76f3b8284623
RahulThakare-28 <rahulthakare6444@gmail.com>
2026-03-29 12:06:25 +0530

    UI for AI testing

---
+++ app.py
@@ -0,0 +1,51 @@
+import streamlit as st
+from main import build_vector_store, Retriever
+from llm_models.groq_llm import get_llm_chain
+
+@st.cache_resource
+def load_system():
+    vector_store, embedder = build_vector_store()
+    retriever = Retriever(vector_store, embedder)
+    chain = get_llm_chain()
+    return retriever, chain
+
+retriever, chain = load_system()
+
+# ---------------- UI ----------------
+st.set_page_config(page_title="IT-Services", layout="wide")
+st.write('IT-Services Company Knowledge Base')
+
+question = st.text_input('Ask a question...')
+submit = st.button('Submit')
+
+# ---------------- Logic ----------------
+
+if submit and question.strip():  
+
+    try:
+        # Step 1: Retrieve
+        results = retriever.invoke(question)
+
+        # Step 2: Build context
+        context = "\n".join([
+            str(doc["content"]) if isinstance(doc, dict)
+            else str(doc.page_content)
+            for doc in results
+        ])
+
+        # Step 3: LLM invoke
+        response = chain.invoke({
+            "context": context,
+            "question": question
+        })
+
+        answer = response.content if hasattr(response, "content") else str(response)
+
+        st.write(answer)
+
+    except Exception as e:
+        st.error(f"❌ Error: {e}")
+
+
+elif submit:
+    st.warning("Please enter a question...!!!")
